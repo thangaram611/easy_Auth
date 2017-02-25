7358715d938c37841cafcedb2ef44620fe35837f
thangaram611 <thangaram611@gmail.com>
2017-02-25 20:04:22 +0530

    add signup, signin with mlab

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
-var express = require('express')
-var path = require('path')
-var bodyParser = require('body-parser')
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var mongojs = require('mongojs');
 
-var mongojs = require('mongojs')
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
+var app = express();
+var port = 8080;
 
-var app = express()
-var port = 8080
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static('static'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('static'));
 
 app.get('/', function(req, res) {
-    res.send(index.html)
-})
+	res.send(index.html);
+});
 
-app.listen(port)
-console.log('server started at port:' + port)
+var db = mongojs('mongodb://thangaram611:kannan@ds161209.mlab.com:61209/db_auth611');
+var registrations = db.collection('registrations');
+app.post('/register', function(req, res) {
+	console.log(req.body);
+	registrations.insert(req.body, function(err){
+		if(err){
+			console.log(err);
+		}
+		else{
+			console.log("no error in insertion");
+			res.send(true);
+		}
+	});
+});
+app.post('/checkregistered', function(req, res) {
+	registrations.findOne({ 'email': req.email }, function(err, doc) {
+		if (err) {
+			console.log(err);
+		} else if (doc) {
+			if (doc.pass == req.pass) {
+				res.send(true);
+			} else {
+				res.send(false);
+			}
+		} else {
+			res.send(false);
+		}
+	});
+});
+
+app.listen(port);
+console.log('server started at port:' + port);
--- static/res/js/main.js
+++ static/res/js/main.js
@@ -1,10 +1,41 @@
 $(document).ready(function() {
 	$('.message a').click(function(e) {
 		e.preventDefault();
-		console.log('nandini');
 		$('form').animate({
 			height: "toggle",
 			opacity: "toggle"
 		}, "slow");
+	});
+	$('form#register').submit(function(e) {
+		e.preventDefault();
+		var req = {};
+		$.each($('#register').serializeArray(), function(i, field) {
+			req[field.name] = field.value;
+		});
+		$.post('/register', req, function(data) {
+			console.log(data);
+			if(data){
+				console.log('done');
+			}
+			else{
+				console.log('notdone');	
+			}
+		});
+	});
+	$('form#login').submit(function(e) {
+		e.preventDefault();
+		var req = {};
+		$.each($('#login').serializeArray(), function(i, field) {
+			req[field.name] = field.value;
+		});
+		$.post('/checkregistered', req, function(data) {
+			console.log(data);
+			if(data){
+				console.log('done');
+			}
+			else{
+				console.log('notdone');
+			}
+		});
 	});
 });
