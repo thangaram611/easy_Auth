7bc0f73552dc7a713a7d2c2eaaa72ecfd5735538
thangaram611 <thangaram611@gmail.com>
2017-02-26 07:57:44 +0530

    add two factor authentication

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var mongojs = require('mongojs');
+var speakeasy = require("speakeasy");
+var QRCode = require('qrcode');
 
 var app = express();
 var port = 8080;
@@ -9,31 +11,34 @@ var port = 8080;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('static'));
-
+// rendering html
 app.get('/', function(req, res) {
 	res.send(index.html);
 });
-
+// db connection
 var db = mongojs('mongodb://thangaram611:kannan@ds161209.mlab.com:61209/db_auth611');
 var registrations = db.collection('registrations');
+var secrets = db.collection('secrets');
+// registration and login
 app.post('/register', function(req, res) {
 	console.log(req.body);
-	registrations.insert(req.body, function(err){
-		if(err){
+	global.email = req.body.email;
+	registrations.insert(req.body, function(err) {
+		if (err) {
 			console.log(err);
-		}
-		else{
+		} else {
 			console.log("no error in insertion");
 			res.send(true);
 		}
 	});
 });
 app.post('/checkregistered', function(req, res) {
-	registrations.findOne({ 'email': req.email }, function(err, doc) {
+	global.email = req.body.email
+	registrations.findOne({ 'email': email }, function(err, doc) {
 		if (err) {
 			console.log(err);
 		} else if (doc) {
-			if (doc.pass == req.pass) {
+			if (doc.pass == req.body.pass) {
 				res.send(true);
 			} else {
 				res.send(false);
@@ -43,6 +48,37 @@ app.post('/checkregistered', function(req, res) {
 		}
 	});
 });
-
+// QRcode generation
+app.post('/qrcode', function(req, res) {
+	var secret = speakeasy.generateSecret({ length: 20 });
+	console.log(secret.base32);
+	secrets.insert({ "email": req.body.email, "secretkey": secret.base32 }, function(err) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log("secret inserted")
+		}
+	});
+	QRCode.toDataURL(secret.otpauth_url, function(err, data_url) {
+		res.send(data_url);
+	});
+});
+// token handling
+app.post('/checktoken', function(req, res) {
+	console.log(global.email);
+	secrets.findOne({ 'email': global.email }, function(err, doc) {
+		console.log(doc);
+		var verified = false;
+		var token = speakeasy.totp({
+			secret: doc.secret,
+			encoding: 'base32'
+		});
+		console.log(token);
+		if(token == req.body.token){
+			verified = true;
+		}
+		res.send(verified);
+	});
+});
 app.listen(port);
 console.log('server started at port:' + port);
--- static/res/js/main.js
+++ static/res/js/main.js
@@ -1,27 +1,34 @@
 $(document).ready(function() {
 	$('.message a').click(function(e) {
 		e.preventDefault();
-		$('form').animate({
+		$('.login-page form').animate({
 			height: "toggle",
 			opacity: "toggle"
 		}, "slow");
 	});
+	// register form submit
 	$('form#register').submit(function(e) {
 		e.preventDefault();
 		var req = {};
 		$.each($('#register').serializeArray(), function(i, field) {
 			req[field.name] = field.value;
 		});
+		var email = req.email;
 		$.post('/register', req, function(data) {
-			console.log(data);
-			if(data){
+			if (data) {
 				console.log('done');
-			}
-			else{
-				console.log('notdone');	
+				var req = {"email": email};
+				$.post('/qrcode', req, function(data) {
+					$('.login-page').hide();
+					$('.twofactor-page').addClass('visible');
+					$('#qrcode img').attr('src', data);
+				});
+			} else {
+				console.log('notdone');
 			}
 		});
 	});
+	// login form submit
 	$('form#login').submit(function(e) {
 		e.preventDefault();
 		var req = {};
@@ -30,12 +37,31 @@ $(document).ready(function() {
 		});
 		$.post('/checkregistered', req, function(data) {
 			console.log(data);
-			if(data){
+			if (data) {
 				console.log('done');
-			}
-			else{
+				$('.login-page').hide();
+				$('.twofactor-page').addClass('visible');
+				$('#qrcode').hide();
+				$('.token').addClass('visible');
+			} else {
 				console.log('notdone');
 			}
+		});
+	});
+	$('#qrcode_button').click(function(e) {
+		e.preventDefault();
+		$('#qrcode').hide();
+		$('.token').addClass('visible');
+	});
+	$('form#token').submit(function(e) {
+		e.preventDefault();
+		var req = {};
+		$.each($('#token').serializeArray(), function(i, field) {
+			req[field.name] = field.value;
+		});
+		console.log(req);
+		$.post('/checktoken', req, function(data) {
+			console.log(data + "token confirmed");
 		});
 	});
 });
